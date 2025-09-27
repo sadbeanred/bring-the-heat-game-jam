c92cff45999be702c35f5e2fd67175fd4e3de71c
sadbeanred <tasssuderman@outlook.com>
2025-09-27 13:32:49 -0600

    added configurations

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.height = 768
+    t.window.width = 1024
+    t.window.resizable = true
+    t.window.vsync = 0
+    t.window.title = "Bring The Heat"
+end
--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ function love.load()
 
     world = wf.newWorld(0, 500)
 
+
     player = world:newRectangleCollider(350, 100, 80, 80)
     ground = world:newRectangleCollider(100, 400, 600, 100)
     wall = world:newRectangleCollider(300, 100, 100, 50)
@@ -32,5 +33,12 @@ function love.draw()
 
 end
 
-function love.keypressed(key)
+function resize (w, h)
+	local w1, h1 = window.width, window.height
+	local scale = math.min (w/w1, h/h1)
+	window.translateX, window.translateY, window.scale = (w-w1*scale)/2, (h-h1*scale)/2, scale
+end
+
+function love.resize (w, h)
+	resize (w, h)
 end
