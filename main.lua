4fdea2786cf5457cf4c1410f7971140374a9d898
Ethan Hrytsak <hrytsak2534@saskpolytech.ca>
2025-09-27 13:53:44 -0600

    added multiple players

--- main.lua
+++ main.lua
@@ -1,30 +1,44 @@
+require "player"
+
+iceConfig = {
+    left = "left",
+    up = "up",
+    down = "down",
+    right = "right",
+}
+fireConfig = {
+    up = "w",
+    left = "a",
+    down = "s",
+    right = "d",
+}
+IcePlayer = {}
+FirePlayer = {}
+
+
+
 function love.load()
     wf = require "lib/windfield"
 
     world = wf.newWorld(0, 500)
 
-    player = world:newRectangleCollider(350, 100, 80, 80)
     ground = world:newRectangleCollider(100, 400, 600, 100)
     wall = world:newRectangleCollider(300, 100, 100, 50)
     wall2 = world:newRectangleCollider(500, 100, 100, 50)
+
     ground:setType("static")
     wall:setType("static")
     wall2:setType("static")
+    IcePlayer = Player:new(iceConfig)
+    FirePlayer = Player:new(fireConfig)
 end
 
 function love.update(dt)
-    local px, py = player:getLinearVelocity()
-    if love.keyboard.isDown("left") and px > -300 then
-        player:applyLinearImpulse(-5000, 0)
-    end
-    if love.keyboard.isDown("right") and px < 300 then
-        player:applyLinearImpulse(5000, 0)
-    end
-    if love.keyboard.isDown("up") and py == 0 then
-        player:applyLinearImpulse(0, -5000)
-    end
+    IcePlayer:update(dt)
+    FirePlayer:update(dt)
     world:update(dt)
 end
+
 
 function love.draw()
     world:draw()
---
+++ player.lua
@@ -0,0 +1,24 @@
+Player = {}
+Player.__index = Player
+
+
+function Player:new(config)
+    local obj = setmetatable({}, self)
+    obj.config = config
+    obj.collider = world:newRectangleCollider(350, 100, 80, 80)
+    return obj
+end
+
+
+function Player:update(dt)
+    local px, py = self.collider:getLinearVelocity()
+    if love.keyboard.isDown(self.config.left) and px > -300 then
+        self.collider:applyLinearImpulse(-5000, 0)
+    end
+    if love.keyboard.isDown(self.config.right) and px < 300 then
+        self.collider:applyLinearImpulse(5000, 0)
+    end
+    if love.keyboard.isDown(self.config.up) and py == 0 then
+        self.collider:applyLinearImpulse(0, -5000)
+    end
+end
