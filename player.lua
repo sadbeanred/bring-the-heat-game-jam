c30a325ae3db715c46a53d47433b3476acadd490
sadbeanred <tasssuderman@outlook.com>
2025-09-27 22:51:06 -0600

    small cleanup

--- player.lua
+++ player.lua
@@ -1,7 +1,5 @@
 require "bullet"
 Player = {
-    -- facing = {x=0,y=0} ,
-    -- bullets = {}
     size = {
         x = 40,
         y = 40
@@ -9,13 +7,16 @@ Player = {
 }
 Player.__index = Player
 
-
 RESPAWN_INVULNERABILITY_TIME = 1
 
-xAcceleration = 50
-xMaxSpeed = 300
-yAcceleration = 150
-sprintFactor = 2
+XAcceleration = 50
+XMaxSpeed = 300
+LeftXDeadZone = 0.3
+RightXDeadZone = 0.3
+LeftYDeadZone = 0.3
+RightYDeadZone = 0.3
+YAcceleration = 150
+SprintFactor = 2
 
 function Player:new(config)
     local obj = setmetatable({}, self)
@@ -34,38 +35,50 @@ end
 
 function Player:update(dt)
     self.invulnerableTime = math.max(self.invulnerableTime - dt, 0)
+    self:manageGamepadInputs(dt)
+    self:manageKeyboardInputs()
+    self:updateBullets(dt)
+    self:handleCollision()
+end
+
+function Player:manageGamepadInputs(dt)
     local px, py = self.collider:getLinearVelocity()
     local joysticks = love.joystick.getJoysticks()
-
-    if #joysticks > 0 and joysticks[self.config.gpIndex]:isGamepad() then
+    
+    if #joysticks > self.config.gpIndex and joysticks[self.config.gpIndex]:isGamepad() then
         local joystick = joysticks[self.config.gpIndex]
         local axisX = joystick:getGamepadAxis(self.config.joystickLeft)
         local axisY = joystick:getGamepadAxis(self.config.joystickUp)
         local lookX = joystick:getGamepadAxis(self.config.lookLeft)
         local lookY = joystick:getGamepadAxis(self.config.lookUp)
-        if axisX > 0.3 then
+        local axisTrigger = joystick:getGamepadAxis(self.config.shootButton)
+        local jumpButtonPressed = joystick:isGamepadDown(self.config.jumpButton)
+
+        if axisX > LeftXDeadZone then
             self:tryMoveHorizontal(axisX, joystick:isGamepadDown(self.config.sprintButton))
         end
-        if axisX < -0.3 then
+        if axisX < -1 * LeftXDeadZone then
             self:tryMoveHorizontal(axisX, joystick:isGamepadDown(self.config.sprintButton))
         end
-        if axisY < -0.8 then
+        if axisY < -1 * LeftYDeadZone then
             self:tryJump(axisY, dt)
         end
-        if math.abs(lookX) > 0.3 or math.abs(lookY) > 0.3 then
+        if math.abs(lookX) > RightXDeadZone or math.abs(RightYDeadZone) > 0.3 then
             self.facing.x = lookX
             self.facing.y = lookY
         end
-        local aButton = joystick:isGamepadDown(self.config.jumpButton)
-        if aButton and py == 0 then
+        if jumpButtonPressed and py == 0 then
             self.collider:applyLinearImpulse(0, -5000)
         end
 
-        local trigger = joystick:getGamepadAxis(self.config.shootButton)
-        if trigger > 0.5 then
+        if axisTrigger > 0.5 then
             self:tryShoot(dt)
         end
     end
+end
+
+function Player:manageKeyboardInputs()
+    local px, py = self.collider:getLinearVelocity()
     if love.keyboard.isDown(self.config.left) and px > -300 then
         self.facing.x = -1
         self.collider:applyLinearImpulse(-1000, 0)
@@ -77,7 +90,9 @@ function Player:update(dt)
     if love.keyboard.isDown(self.config.up) and py == 0 then
         self.collider:applyLinearImpulse(0, -5000)
     end
+end
 
+function Player:updateBullets(dt)
     local removableItems = {}
     if self.bullets then
         for i, bullet in ipairs(self.bullets) do
@@ -94,20 +109,12 @@ function Player:update(dt)
     self:handleCollision()
 end
 
-function Player:handleCollision()
-    if self.collider:enter("KillField") then
-        self.collider:setType("static")
-        self:respawn()
-        self.collider:setType("dynamic")
-    end
-end
-
 function Player:tryMoveHorizontal(movementAmount, isSprinting)
     local px, py = self.collider:getLinearVelocity()
-    local maxSpeed = isSprinting and (xMaxSpeed * sprintFactor) or xMaxSpeed
+    local maxSpeed = isSprinting and (XMaxSpeed * SprintFactor) or XMaxSpeed
     if(math.abs(px) < maxSpeed) then
         --self.facing.x =  1
-        local accel = isSprinting and (xAcceleration * sprintFactor) or xAcceleration
+        local accel = isSprinting and (XAcceleration * SprintFactor) or XAcceleration
         self.collider:applyLinearImpulse(movementAmount * accel, 0)
     end
 end
@@ -117,7 +124,7 @@ function Player:tryJump(movementAmount, dt)
     if self.onGround then
         self.jumpCooldown = self.jumpCooldown + dt
         if self.jumpCooldown < 0.1 then
-            self.collider:applyLinearImpulse(0, movementAmount * yAcceleration * dt * 500)
+            self.collider:applyLinearImpulse(0, movementAmount * YAcceleration * dt * 500)
         else
             self.onGround = false
             self.jumpCooldown = 0
@@ -127,7 +134,9 @@ end
 
 function Player:handleCollision()
     if self.collider:enter("KillField") then
+        self.collider:setType("static")
         self:respawn()
+        self.collider:setType("dynamic")
     end
     if self.collider:enter("Terrain") then
         self.onGround = true
